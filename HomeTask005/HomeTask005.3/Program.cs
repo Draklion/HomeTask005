1c04ddcb75c77f9422fa0a2e9e1ca22ea3a3f179
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-19 01:05:57 +0500

    HomeTask005.3 completed.

--- HomeTask005/HomeTask005.3/Program.cs
+++ HomeTask005/HomeTask005.3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+try
+{
+  CW("Задание 3. Программа, которая задает одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].");
+
+  int[] array = new int[123];
+
+  Random rand = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(1, 300);
+
+  int count = 0;
+  CW("Исходный массив: [" + string.Join(",", array) + "]");
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+      count++;
+    }
+  }
+  CW($"Количество элементов массива, значения которых лежат в отрезке [10,99]: {count}");
+}
+catch { CW("Вы ввели некорректное значение."); }
