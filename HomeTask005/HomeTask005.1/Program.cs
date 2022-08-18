c72837fb943a32ffb2fb96843a91a567f206d229
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-19 00:27:30 +0500

    HomeTask005.1 completed.

--- HomeTask005/HomeTask005.1/Program.cs
+++ HomeTask005/HomeTask005.1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+try
+{
+  CW("Задание 1. Программа, которая заменяет элементы массива: положительные элементы замените на соответствующие отрицательные, и наоборот.");
+
+  int[] array = new int[5];
+
+  Random rand = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(-9, 10);
+
+  CW("Исходный массив: [" + string.Join(",", array) + "]");
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = -array[i];
+  }
+  CW("Массив с противоположными элементами: [" + string.Join(",", array) + "]");
+}
+catch { CW("Вы ввели некорректное значение."); }
