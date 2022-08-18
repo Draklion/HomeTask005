f8c316c044edc1ea0ed80f6d2f857325566e920f
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-19 02:20:56 +0500

    HomeTask005.7 completed.

--- HomeTask005/HomeTask005.7/Program.cs
+++ HomeTask005/HomeTask005.7/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  CW("Задание 7. Программа, которая находит разницу между максимальным и минимальным элементов массива.");
+  CW("Введите элементы массива: ");
+
+  double[] array = new double[5];
+  double max = 0;
+  double min = 0;
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = double.Parse(CR());
+
+  max = array[0];
+  min = array[0];
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (max < array[i])
+      max = array[i];
+    else if (min > array[i])
+      min = array[i];
+  }
+  CW(max + " " + min);
+  CW($"Разницу между максимальным и минимальным элементов массива : {max - min}");
+}
+catch { CW("Вы ввели некорректное значение."); }
