9a68a5a80d25ab76ff5e6a66666e2f41e667874c
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-19 00:54:21 +0500

    HomeTask005.2 completed.

--- HomeTask005/HomeTask005.2/Program.cs
+++ HomeTask005/HomeTask005.2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  CW("Задание 2. Программа, которая определяет, присутствует ли заданное число в массиве.");
+  CW("Введите число для поиска в массиве: ");
+
+  int number = Int32.Parse(CR());
+  int[] array = new int[10];
+
+  Random rand = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(1, 10);
+  int count = 0;
+  CW("Исходный массив: [" + string.Join(",", array) + "]");
+  CW($"Присутствует ли заданное число в массиве?");
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] == number)
+    {
+      count++;
+      break;
+    }
+  }
+  CW(count > 0 ? "Да" : "Нет");
+}
+catch { CW("Вы ввели некорректное значение."); }
