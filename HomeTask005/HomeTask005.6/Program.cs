201ffe750e038138e663e823d37fa7f2b24c9d55
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-19 02:06:20 +0500

    HomeTask005.6 completed.

--- HomeTask005/HomeTask005.6/Program.cs
+++ HomeTask005/HomeTask005.6/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+try
+{
+  CW("Задание 6. Программа, которая сумму элементов, стоящих на нечётных позициях.");
+
+  int[] array = new int[6];
+
+  Random rand = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(-100, 100);
+
+  int sum = 0;
+  CW("Исходный массив: [" + string.Join(",", array) + "]");
+
+  for (int i = 0; i < array.Length; i += 2)
+  {
+    sum = sum + array[i];
+  }
+  CW($"Cуммf элементов, стоящих на нечётных позициях: {sum}");
+}
+catch { CW("Вы ввели некорректное значение."); }
