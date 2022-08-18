2d87821b8f2741da5f9ebd4c09b192e61d6b32e0
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-19 02:00:47 +0500

    HomeTask005.5 completed.

--- HomeTask005/HomeTask005.5/Program.cs
+++ HomeTask005/HomeTask005.5/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+try
+{
+  CW("Задание 5. Программа, которая которая покажет количество чётных чисел в массиве.");
+
+  int[] array = new int[10];
+
+  Random rand = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(100, 1000);
+
+  int count = 0;
+  CW("Исходный массив: [" + string.Join(",", array) + "]");
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] % 2 == 0)
+    {
+      count++;
+    }
+  }
+  CW($"Количество чётных чисел в массиве: {count}");
+}
+catch { CW("Вы ввели некорректное значение."); }
